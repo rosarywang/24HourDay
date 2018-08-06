b44f9e8b30c2a257e2e86bd7f45c0ec3c35d7f53
Rosary Wang <rosaryywang@gmail.com>
2018-08-05 23:28:41 -0400

    attempt to remove effect of passOutEvent
    Remove passOut animation
    Fix farmer's facingDirection
    Add some comments

--- 24HoursDay/ModEntry.cs
+++ 24HoursDay/ModEntry.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using Netcode;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -17,7 +18,11 @@ namespace _24HourDay
 
         private int PreCollapseHealth;
 
-        private bool StartPassOut;
+        private bool StartToPassOut;
+
+        private bool HasPassOut;
+
+        private int facingDirection = 0;
 
         public override void Entry(IModHelper helper)
         {
@@ -31,7 +36,9 @@ namespace _24HourDay
 
         public void TimeEvents_AfterDayStarted(object sender, EventArgs e)
         {
-            StartPassOut = false;
+            StartToPassOut = false;
+            HasPassOut = false;
+            //Reinstate stamina and health if it was higher when passed out at night
             if (Game1.player.stamina < this.PreCollapseStamina)
                 Game1.player.stamina = this.PreCollapseStamina;
             if (Game1.player.health < this.PreCollapseHealth)
@@ -41,11 +48,11 @@ namespace _24HourDay
 
         private void TimeEvents_TimeOfDayChanged(object sender, EventArgs e)
         {
+            //Add clock shake animation at every hour between 2 AM and 6 AM
             if (Game1.timeOfDay > 2550 && Game1.timeOfDay < 3000 && Game1.timeOfDay % 100 == 0)
             {
                 Game1.dayTimeMoneyBox.timeShakeTimer = 2000;
                 Game1.player.doEmote(24);
-                Game1.player.yJumpVelocity = -2f;
             }
         }
 
@@ -60,52 +67,145 @@ namespace _24HourDay
             {
                 this.Monitor.Log($"FourthUpdateTick called. Current time {Game1.timeOfDay.ToString()}");
 
-                //TODO: Remove StartToPassOut from animation 
-                //TODO: Reinstate animation and movement
-
-                if (!Game1.newDay)
+                if (!Game1.newDay && StartToPassOut == false)
                 {
-                    if (Game1.timeOfDay >= 3000 && StartPassOut == false)
+                    Farmer farmer = Game1.player;
+
+                    //Running out of stamina will still passed out
+                    if ((double)Game1.player.stamina > -15.0)
                     {
-                        this.Monitor.Log($"FourthUpdateTick called. Attempt to restart the day");
-                        StartPassOut = true;
-                        Game1.dayTimeMoneyBox.timeShakeTimer = 2000;
-                        Game1.player.freezePause = 7000;
-                        startPassOut();
-                        this.Monitor.Log($"FourthUpdateTick ended.");
+                        //Remove StartToPassOut from animation 
+                        FarmerSprite sprite = (FarmerSprite)farmer.Sprite;
+                        if (sprite.CurrentAnimation != null && 
+                            sprite.currentAnimation.Any(x => x.frameBehavior == Farmer.passOutFromTired))
+                        {
+                            sprite.StopAnimation();
+                            this.Monitor.Log($"sprite stop animation.");
+                            sprite.PauseForSingleAnimation = false;
+                            farmer.freezePause = 0;
+                            farmer.CanMove = true;
+                            farmer.forceCanMove();
+                            this.Monitor.Log($"player forceCanMove");
+                            HasPassOut = true;
+                            
+                        }
+
+                        //TODO: make sure time pass
+
+                        //Fix facingDirection caused by startToPassOut animation
+                        if (HasPassOut && !farmer.FarmerSprite.PauseForSingleAnimation && !farmer.UsingTool && farmer.CanMove || (Game1.eventUp && farmer.isRidingHorse()))
+                        {
+                            this.Monitor.Log($"Set facingDirection when button pressed");
+                            if (this.Helper.Input.IsDown(SButton.Right) || this.Helper.Input.IsDown(SButton.LeftThumbstickRight) || this.Helper.Input.IsDown(SButton.DPadRight) || this.Helper.Input.IsDown(SButton.D))
+                            {
+                                if (this.facingDirection != 1)
+                                {
+                                    this.Monitor.Log($"change FacingDirection 1");
+                                    this.facingDirection = 1;
+                                }
+                            }
+                            else if (this.Helper.Input.IsDown(SButton.Left) || this.Helper.Input.IsDown(SButton.LeftThumbstickLeft) || this.Helper.Input.IsDown(SButton.DPadLeft) || this.Helper.Input.IsDown(SButton.A))
+                            {
+                                if (this.facingDirection != 3)
+                                {
+                                    this.Monitor.Log($"change FacingDirection 3");
+                                    this.facingDirection = 3;
+                                }
+                            }
+                            else if (this.Helper.Input.IsDown(SButton.Up) || this.Helper.Input.IsDown(SButton.LeftThumbstickUp) || this.Helper.Input.IsDown(SButton.DPadUp) || this.Helper.Input.IsDown(SButton.W))
+                            {
+                                if (this.facingDirection != 0)
+                                {
+                                    this.Monitor.Log($"change FacingDirection 0");
+                                    this.facingDirection = 0;
+                                }
+                            }
+                            else if (this.Helper.Input.IsDown(SButton.Down) || this.Helper.Input.IsDown(SButton.LeftThumbstickDown) || this.Helper.Input.IsDown(SButton.DPadDown) || this.Helper.Input.IsDown(SButton.S))
+                            {
+                                if (this.facingDirection != 2)
+                                {
+                                    this.Monitor.Log($"change FacingDirection 2");
+                                    this.facingDirection = 2;
+                                }
+                            }
+                            this.Monitor.Log($"facingDirection {this.facingDirection.ToString()}");
+                            if (farmer.FacingDirection != this.facingDirection)
+                            {
+                                farmer.FacingDirection = this.facingDirection;
+                                animateMovement(farmer, Game1.currentGameTime);
+                                //TODO: Reinstate animation and movement
+                            }
+                        }
+
+                        //Harvest Moon style day cycle
+                        //Passed out at 6 AM and return home
+                        //No money lost
+                        if (Game1.timeOfDay >= 3000)
+                        {
+                            this.Monitor.Log($"FourthUpdateTick called. Attempt to restart the day");
+                            StartToPassOut = true;
+                            Game1.dayTimeMoneyBox.timeShakeTimer = 2000;
+                            farmer.freezePause = 7000;
+                            startPassOut();
+                            this.Monitor.Log($"FourthUpdateTick ended.");
+                        }
+
                     }
                 }
             }
         }
 
-        public void completelyStopAnimatingOrDoingAction()
+        //Based on StardewValley.Farmer.updateMovementAnimation()
+        public void animateMovement(Farmer who, GameTime time)
         {
-            this.Monitor.Log($"completelyStopAnimatingOrDoingAction called.");
-            Farmer farmer = Game1.player;
-            farmer.CanMove = false;
-            if (farmer.UsingTool)
-                farmer.EndUsingTool();
-            if (farmer.usingSlingshot && farmer.CurrentTool is Slingshot)
-                (farmer.CurrentTool as Slingshot).finish();
-            farmer.UsingTool = false;
-            farmer.isEating = false;
-            farmer.FarmerSprite.PauseForSingleAnimation = false;
-            farmer.usingSlingshot = false;
-            farmer.canReleaseTool = false;
-            farmer.Halt();
-            farmer.Sprite.StopAnimation();
-            if (farmer.CurrentTool is MeleeWeapon)
-                (farmer.CurrentTool as MeleeWeapon).isOnSpecial = false;
-            farmer.stopJittering();
-            this.Monitor.Log($"completelyStopAnimatingOrDoingAction ended.");
+            bool flag = who.ActiveObject != null && !Game1.eventUp;
+            if (who.isRidingHorse() && !who.mount.dismounting.Value)
+                who.showRiding();
+            else if (who.FacingDirection == 3 && who.running && !flag)
+                who.FarmerSprite.animate(56, time);
+            else if (who.FacingDirection == 1 && who.running && !flag)
+                who.FarmerSprite.animate(40, time);
+            else if (who.FacingDirection == 0 && who.running && !flag)
+                who.FarmerSprite.animate(48, time);
+            else if (who.FacingDirection == 2 && who.running && !flag)
+                who.FarmerSprite.animate(32, time);
+            else if (who.FacingDirection == 3 && who.running)
+                who.FarmerSprite.animate(152, time);
+            else if (who.FacingDirection == 1 && who.running)
+                who.FarmerSprite.animate(136, time);
+            else if (who.FacingDirection == 0 && who.running)
+                who.FarmerSprite.animate(144, time);
+            else if (who.FacingDirection == 2 && who.running)
+                who.FarmerSprite.animate(128, time);
+            else if (who.FacingDirection == 3 && !flag)
+                who.FarmerSprite.animate(24, time);
+            else if (who.FacingDirection == 1 && !flag)
+                who.FarmerSprite.animate(8, time);
+            else if (who.FacingDirection == 0 && !flag)
+                who.FarmerSprite.animate(16, time);
+            else if (who.FacingDirection == 2 && !flag)
+                who.FarmerSprite.animate(0, time);
+            else if (who.FacingDirection == 3)
+                who.FarmerSprite.animate(120, time);
+            else if (who.FacingDirection == 1)
+                who.FarmerSprite.animate(104, time);
+            else if (who.FacingDirection == 0)
+                who.FarmerSprite.animate(112, time);
+            else if (who.FacingDirection == 2)
+                who.FarmerSprite.animate(96, time);
+            else if (flag)
+                who.showCarrying();
+            else
+                who.showNotCarrying();
         }
 
+        //Based on StardewValley.Farmer.performPassOut()
         public void startPassOut()
         {
             this.Monitor.Log($"startPassOut called.");
             Farmer farmer = Game1.player;
             farmer.faceDirection(2);
-            completelyStopAnimatingOrDoingAction();
+            farmer.completelyStopAnimatingOrDoingAction();
             farmer.CanMove = false;
             FarmerSprite sprite = (FarmerSprite)farmer.Sprite;
             sprite.PauseForSingleAnimation = true;
@@ -119,6 +219,7 @@ namespace _24HourDay
             this.Monitor.Log($"startPassOut ended.");
         }
 
+        //Based on StardewValley.FarmerSprite animation# 293
         public FarmerSprite.AnimationFrame[] getPassOutAnimation()
         {
             this.Monitor.Log($"getPassOutAnimation called.");
@@ -136,6 +237,7 @@ namespace _24HourDay
 
         }
 
+        //Based on StardewValley.Farmer.passOutFromTired()
         public void passOut(Farmer who)
         {
             this.Monitor.Log($"passOut called.");
@@ -146,7 +248,7 @@ namespace _24HourDay
                 Game1.activeClickableMenu.emergencyShutDown();
                 Game1.exitActiveMenu();
             }
-            completelyStopAnimatingOrDoingAction();
+            who.completelyStopAnimatingOrDoingAction();
             if (who.bathingClothes.Value)
                 who.changeOutOfSwimSuit();
             who.swimming.Value = false;
